2773cfe07f0a95c9b1b07282e47cf5561128aec7
junyuanoh <102602516+junyuanoh@users.noreply.github.com>
2022-03-31 18:13:46 +0800

    modified initial setup
    check for module and get status for module

--- initialsetup.ps1
+++ initialsetup.ps1
@@ -1,4 +1,64 @@
 Write-Host "Starting..."
-Install-Module AzureAD -Scope CurrentUser;
-Install-Module -Name MSOnline -Scope CurrentUser;
+Write-Host ""
+Write-Host "Installing AzureAD Module..."
+
+#Check if module is installed & imported, do it if not:
+$module = "AzureAD"
+if((Get-Module -Name $module) -eq $null) {
+    #Check if module is installed:
+    if (Get-Module -ListAvailable -Name $module) {
+        Write-Output "$module module is already installed";
+        #Check if module is imported:
+        if((Get-Module -Name $module) -ne $null) {
+            Write-Output " and is already imported (i.e. its cmdlets are ready/available to be used.)";
+            Write-Output "Installation Path: `n`t" (Get-Module -Name $module).Path;
+        } else {
+            Write-Output ", however, it is NOT imported yet - importing it now";
+            Import-Module $module;
+        }
+    }else{
+        Write-Output "$module module is NOT installed - installing it now"
+        try {
+            Install-Module -Name $module -AllowClobber -Confirm:$False -Force -Scope CurrentUser #suppressed prompt.
+        }
+        catch [Exception] {
+            $_.message 
+            exit
+        }
+        Write-Output "$module installed successfully - importing it now";
+        Import-Module $module;
+    }
+};
+
+#Check if module is installed & imported, do it if not:
+$module = "MSOnline"
+if((Get-Module -Name $module) -eq $null) {
+    #Check if module is installed:
+    if (Get-Module -ListAvailable -Name $module) {
+        Write-Output "$module module is already installed";
+        #Check if module is imported:
+        if((Get-Module -Name $module) -ne $null) {
+            Write-Output " and is already imported (i.e. its cmdlets are ready/available to be used.)";
+            Write-Output "Installation Path: `n`t" (Get-Module -Name $module).Path;
+        } else {
+            Write-Output ", however, it is NOT imported yet - importing it now";
+            Import-Module $module;
+        }
+    }else{
+        Write-Output "$module module is NOT installed - installing it now"
+        try {
+            Install-Module -Name $module -AllowClobber -Confirm:$False -Force -Scope CurrentUser #suppressed prompt.
+        }
+        catch [Exception] {
+            $_.message 
+            exit
+        }
+        Write-Output "$module installed successfully - importing it now";
+        Import-Module $module;
+    }
+};
+Write-Host ""
+Write-Host "Completed"
+Write-Host ""
+
 Get-InstalledModule
