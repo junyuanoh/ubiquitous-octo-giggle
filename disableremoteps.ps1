cac19924f2c890ae17785db05116e5bdc60d8789
junyuanoh <102602516+junyuanoh@users.noreply.github.com>
2022-04-01 23:46:21 +0800

    refined disableremotePS.ps1

--- disableremoteps.ps1
+++ disableremoteps.ps1
@@ -2,6 +2,6 @@ Write-Host "Disables remotePS for a list of user in a .txt file. The text file m
 $location = Read-Host "Enter .txt file location"
 $lines = Get-Content -Path "$location"
 $NPS = Get-Content "$location"
-$NPS | foreach {Set-User -Identity $_ -RemotePowerShellEnabled $false}
-$NPS | foreach {Get-User -Identity $_ | Format-List RemotePowerShellEnabled}
+$NPS | ForEach-Object {Set-User -Identity $_ -RemotePowerShellEnabled $false}
+$NPS | ForEach-Object {Get-User -Identity $_ | Format-List RemotePowerShellEnabled}
 Write-Host "Completed`nRemote PS disabled for $($lines.Count) users."
