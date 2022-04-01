e82fdde96bab34cb7dde6365c03167b8d375c715
junyuanoh <102602516+junyuanoh@users.noreply.github.com>
2022-04-01 21:58:08 +0800

    added exchangeonlinemanagement module

--- initialsetup.ps1
+++ initialsetup.ps1
@@ -1,7 +1,5 @@
-Write-Host "Starting..."
-Write-Host ""
-Write-Host "Installing AzureAD Module..."
-Write-Host ""
+Write-Host "Starting...`n"
+Write-Host "Installing AzureAD Module...`n"
 
 #Check if module is installed & imported, do it if not:
 $module = "AzureAD"
@@ -31,9 +29,7 @@ if((Get-Module -Name $module) -eq $null) {
     }
 };
 
-Write-Host ""
-Write-Host "Installing AzureAD Module..."
-Write-Host ""
+Write-Host "`nInstalling MSOnline Module...`n"
 
 #Check if module is installed & imported, do it if not:
 $module = "MSOnline"
@@ -62,12 +58,45 @@ if((Get-Module -Name $module) -eq $null) {
         Import-Module $module;
     }
 };
+
+Write-Host ""
+Write-Host "Installing ExchangeOnlineManagement Module..."
+Write-Host ""
+
+#Check if module is installed & imported, do it if not:
+$module = "ExchangeOnlineManagement"
+if((Get-Module -Name $module) -eq $null) {
+    #Check if module is installed:
+    if (Get-Module -ListAvailable -Name $module) {
+        Write-Output "$module module is already installed";
+        #Check if module is imported:
+        if((Get-Module -Name $module) -ne $null) {
+            Write-Output " and is already imported (i.e. its cmdlets are ready/available to be used.)";
+            Write-Output "Installation Path: `n`t" (Get-Module -Name $module).Path;
+        } else {
+            Write-Output ", however, it is NOT imported yet - importing it now";
+            Import-Module $module;
+        }
+    }else{
+        Write-Output "$module module is NOT installed - installing it now"
+        try {
+            Install-Module -Name $module -AllowClobber -Confirm:$False -Force -Scope CurrentUser #suppressed prompt.
+        }
+        catch [Exception] {
+            $_.message 
+            exit
+        }
+        Write-Output "$module installed successfully - importing it now";
+        Import-Module $module;
+    }
+};
+
 Write-Host ""
 Get-InstalledModule
+Write-Host "`nCompleted`n"
 
-Write-Host ""
-Write-Host "Completed"
-Write-Host ""
-Write-Host "Installing Exchange Online Powershell Module..."
-
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
+Write-Host "Setting execution policy to remote signed`n"
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+Write-Host "`nCompleted`n."
+Import-Module ExchangeOnlineManagement
+Connect-ExchangeOnlineManagement
