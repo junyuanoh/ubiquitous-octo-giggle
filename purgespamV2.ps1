f0fe12c4c5b47a34b57d456c45fdbafa9f9a1e23
junyuanoh <102602516+junyuanoh@users.noreply.github.com>
2022-04-05 00:44:00 +0800

    testing purgespam 2.0

---
+++ purgespamV2.ps1
@@ -0,0 +1,14 @@
+Write-Host "`nConnecting to IPPS Session (Security & Compliance`n)"
+$userUPN = Read-Host "Please enter your work email"
+Connect-IPPSSession -UserPrincipalName $userUPN
+
+$phish1 = Read-Host "Enter phishing email no.1"
+
+if ($phish1) { 
+    Set-ComplianceSearch -Identity "Phish Mail (JY)" -ContentMatchQuery "from=$phish1" `
+    | Start-ComplianceSearch -Identity "Phish Mail (JY)" -Confirm:$false -Force
+}
+else {
+    $phish1 = Read-Host "Enter phishing email no.1"
+}
+
