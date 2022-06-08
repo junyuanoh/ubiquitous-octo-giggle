c73187b207cfb2523a04ae0e5a298b2a47fcd4a1
junyuanoh <102602516+junyuanoh@users.noreply.github.com>
2022-06-08 16:17:10 +0800

    comment line on enable MFA

--- enableMFA.ps1
+++ enableMFA.ps1
@@ -1,8 +1,10 @@
 Connect-MsolService
+Write-Host "`nEnsure that .csv file used has 'UserPrincipalName' in the first column and row, followed by name@domain.com after each row."
 $csvloc = Read-Host "Enter path to .csv" 
 $users = Import-Csv $csvloc
 
 ### above for input, below for hard code 
+
 
 $users = Import-Csv \\Mac\Home\Desktop\PSScriptCSVs\BulkUpdateMFASampleFile.csv
   
