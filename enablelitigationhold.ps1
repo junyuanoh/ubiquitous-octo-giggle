f5afe8f96a78213e40b163178e1aaf29a0d18452
junyuanoh <102602516+junyuanoh@users.noreply.github.com>
2023-03-30 18:59:24 +0800

    Update enablelitigationhold.ps1
    #get list of litigation hold

--- enablelitigationhold.ps1
+++ enablelitigationhold.ps1
@@ -6,3 +6,13 @@ Enable-Mailbox $upn -AutoExpandingArchive
 Set-Mailbox $upn -LitigationHoldEnabled $true
 Set-Mailbox $upn -RetentionPolicy "Retention Policy_OnlineArchive"
 Get-Mailbox -Identity $upn | FL UserPrincipalName, LitigationHoldEnabled, AutoExpandingArchiveEnabled, ArchiveStatus, RetentionPolicy
+
+#get list of litigation hold
+Connect-ExchangeOnline -UserPrincipalName satsvn_junyuanoh@sats.com.sg
+$csvloc1 = Read-Host "Enter path to .csv (remove quotes)" 
+$csvloc2 = $csvloc1.Replace("`"","")
+$users = Import-Csv $csvloc2 
+foreach($user in $users) {
+    $upn = $user.UserPrincipalName
+    Get-Mailbox -Identity $upn | FL UserPrincipalName, LitigationHoldEnabled
+}
