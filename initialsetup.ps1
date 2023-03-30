d20488d14647263d7c12b924c7d8e7ab1326c9ac
junyuanoh <102602516+junyuanoh@users.noreply.github.com>
2023-03-30 19:42:54 +0800

    Update initialsetup.ps1
    added aip and spo management

--- initialsetup.ps1
+++ initialsetup.ps1
@@ -1,5 +1,8 @@
-$userUPN = Read-Host "Please enter your work email"
 Write-Host "`nStarting...`n"
+Write-Host "`nSetting execution policy to remote signed`n"
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+
 Write-Host "Installing AzureAD Module...`n"
 
 #Check if module is installed & imported, do it if not:
@@ -93,9 +96,71 @@ if((Get-Module -Name $module) -eq $null) {
 };
 
 Write-Host ""
+Write-Host "Installing SPO Management Module..."
+Write-Host ""
+
+#Check if module is installed & imported, do it if not:
+$module = "Microsoft.Online.SharePoint.PowerShell"
+if((Get-Module -Name $module) -eq $null) {
+    #Check if module is installed:
+    if (Get-Module -ListAvailable -Name $module) {
+        Write-Output "$module module is already installed";
+        #Check if module is imported:
+        if((Get-Module -Name $module) -ne $null) {
+            Write-Output " and is already imported (i.e. its cmdlets are ready/available to be used.)";
+            Write-Output "Installation Path: `n`t" (Get-Module -Name $module).Path;
+        } else {
+            Write-Output ", however, it is NOT imported yet - importing it now";
+            Import-Module $module;
+        }
+    }else{
+        Write-Output "$module module is NOT installed - installing it now"
+        try {
+            Install-Module -Name $module -AllowClobber -Confirm:$False -Force -Scope CurrentUser #suppressed prompt.
+        }
+        catch [Exception] {
+            $_.message 
+            exit
+        }
+        Write-Output "$module installed successfully - importing it now";
+        Import-Module $module;
+    }
+};
+
+Write-Host ""
+Write-Host "Installing AIP Service Module..."
+Write-Host ""
+
+#Check if module is installed & imported, do it if not:
+$module = "AIPService"
+if((Get-Module -Name $module) -eq $null) {
+    #Check if module is installed:
+    if (Get-Module -ListAvailable -Name $module) {
+        Write-Output "$module module is already installed";
+        #Check if module is imported:
+        if((Get-Module -Name $module) -ne $null) {
+            Write-Output " and is already imported (i.e. its cmdlets are ready/available to be used.)";
+            Write-Output "Installation Path: `n`t" (Get-Module -Name $module).Path;
+        } else {
+            Write-Output ", however, it is NOT imported yet - importing it now";
+            Import-Module $module;
+        }
+    }else{
+        Write-Output "$module module is NOT installed - installing it now"
+        try {
+            Install-Module -Name $module -AllowClobber -Confirm:$False -Force -Scope CurrentUser #suppressed prompt.
+        }
+        catch [Exception] {
+            $_.message 
+            exit
+        }
+        Write-Output "$module installed successfully - importing it now";
+        Import-Module $module;
+    }
+};
+
+Write-Host ""
 Get-InstalledModule
-Write-Host "`nSetting execution policy to remote signed`n"
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 Write-Host "Completed"
 
 
