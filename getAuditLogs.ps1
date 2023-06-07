ccddda87424b42589420a9a20174be0e452a1674
junyuanoh <102602516+junyuanoh@users.noreply.github.com>
2023-06-07 17:18:15 +0800

    update

--- getAuditLogs.ps1
+++ getAuditLogs.ps1
@@ -1,13 +1,30 @@
 Connect-AzureAD
 
+Function Start-Countdown{   
+    Param(
+        [Int32]$Seconds = 10,
+        [string]$Message = "Pausing for 10 seconds..."
+    )
+    ForEach ($Count in (1..$Seconds))
+    {   Write-Progress -Id 1 -Activity $Message -Status "Waiting for $Seconds seconds, $($Seconds - $Count) left" -PercentComplete (($Count / $Seconds) * 100)
+        Start-Sleep -Seconds 1
+    }
+    Write-Progress -Id 1 -Activity $Message -Status "Completed" -PercentComplete 100 -Completed
+}
+
 $csvloc1 = Read-Host "Enter path to .csv" 
 $csvloc2 = $csvloc1.Replace("`"","")
 $users = Import-Csv $csvloc2
 
 foreach ($user in $users) {
     $upn = $user.UserPrincipalName
-    Get-AzureADAuditDirectoryLogs -Filter "targetResources/any(tr:tr/UserPrincipalName eq '$upn')" | Export-Csv -Path "\\Mac\Home\Desktop\OutputCSV\INC01451783\$upn.csv" -NoTypeInformation
+    $var_storeid = Get-AzureADUser -Filter "userPrincipalName eq '$upn'"
+    $var_objectid = $var_storeid.objectid
+    Start-Countdown -Seconds 15 -Message "Wait 15 seconds before next user..."
+    Get-AzureADAuditDirectoryLogs -Filter "targetResources/any(tr:tr/id eq '$var_objectid')" | Export-Csv -Path "\\Mac\Home\Desktop\OutputCSV\INC000023723557\$upn.csv" -NoTypeInformation
 }
 
 ### 
 
+$var_objectid = Get-AzureADUser -Filter "userPrincipalName eq '$upn'" | Select-Object ObjectId
+Get-AzureADAuditDirectoryLogs -Filter "targetResources/any(tr:tr/id eq '$var_objectid')"
--- getSignInLogs.ps1
+++ getSignInLogs.ps1
@@ -14,7 +14,7 @@ foreach ($user in $users) {
 # https://learn.microsoft.com/en-us/powershell/module/azuread/get-azureadauditsigninlogs?view=azureadps-2.0-preview
 # https://infrasos.com/get-azureadauditsigninlogs-find-sign-in-logs-for-last-30-days-with-powershell/
 
-
+#single IP
 Connect-AzureAD
 
 $csvloc1 = Read-Host "Enter path to .csv" 
@@ -24,7 +24,34 @@ $ipaddress = Read-Host "Enter ipaddress"
 
 foreach ($user in $users) {
     $upn = $user.UserPrincipalName
-    Get-AzureADAuditSignInLogs -Filter "startsWith(userPrincipalName,'$upn')" | Where-Object { $_.IpAddress -eq $ipaddress } | Export-Csv -Path "\\Mac\Home\Desktop\OutputCSV\INC000023753531\sign-in\$upn.csv" -NoTypeInformation
+    Get-AzureADAuditSignInLogs -Filter "startsWith(userPrincipalName,'$upn')" | Where-Object { $_.IpAddress -eq $ipaddress } | Export-Csv -Path "\\Mac\Home\Desktop\OutputCSV\INC000023723557\sign-in\$upn.csv" -NoTypeInformation
 }
 
 
+#multi IP
+Connect-AzureAD
+
+# Define the path to the CSV files
+$userPrincipalNameCSV = Read-Host "Enter path to .csv for UPN" 
+$ipAddressCSV = Read-Host "Enter path to .csv for IP" 
+
+# Read UserPrincipalName CSV file
+$userPrincipalNameData = Import-Csv -Path $userPrincipalNameCSV
+
+# Read IP Address CSV file
+$ipAddressData = Import-Csv -Path $ipAddressCSV
+
+# Loop through each user principal name
+foreach ($user in $userPrincipalNameData) {
+    # Loop through each IP address
+    foreach ($ip in $ipAddressData) {
+        # Create a file name using the convention: userprincipalname_ipaddress.csv
+        $fileName = "$($user.UserPrincipalName)_$($ip.IPAddress).csv"
+        
+        # Export sign-in logs for the specified IP address
+        $signIns = Get-AzureADAuditSignInLogs -Filter "IpAddress eq '$($ip.IPAddress)' and UserPrincipalName eq '$($user.UserPrincipalName)'"
+        
+        # Export the sign-in logs to a CSV file
+        $signIns | Export-Csv -Path "\\Mac\Home\Desktop\OutputCSV\INC000023723557\sign-in\$fileName" -NoTypeInformation
+    }
+}
